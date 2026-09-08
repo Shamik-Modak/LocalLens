c56ff2663eb9a22b25cb40374b9008e9d1a9b645
unknown <shamikmodak5105@gmail.com>
2026-09-08 21:01:37 +0530

    Update frontend UI

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup=async (req, res, next) => {
       if(err){
         return next(err);
       }
-      req.flash("success", "Welcome to WanderLust!");
+      req.flash("success", "Welcome to LocalLens!");
       res.redirect("/listings");
     })
     // console.log(regUser);
@@ -30,7 +30,7 @@ module.exports.renderLogin=(req, res) => {
 }
 
 module.exports.login=async (req, res) => {
-    req.flash("success","Welcome back to WanderLust! You are logged in!")
+    req.flash("success","Welcome back to LocalLens! You are logged in!")
     const redirectUrl=res.locals.redirectUrl || "/listings";
     res.redirect(redirectUrl);
   }
